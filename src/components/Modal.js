45afd6749d51d3a49c9918117a3002037113810d
akeembrisco <briscoakeem@gmail.com>
2023-05-10 18:42:01 -0400

    removed newsletter and added modal template

---
+++ src/components/Modal.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { CustomCarousel } from "./Carousel";
+
+export const ModalComp = ({ displayModal, modalInfo }) => {
+  const handleClose = () => {
+    displayModal(false);
+  };
+  const { description, imgUrls, showModal, subtitle, title, visitUrl } =
+    modalInfo || {};
+
+  return (
+    <>
+      <Modal
+        show={showModal}
+        onHide={handleClose}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header className="custom-modal-header">
+          <Modal.Title className="custom-modal-title">
+            {showModal && <CustomCarousel imgUrls={imgUrls} title={title} />}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="custom-modal-body">
+          {title}
+          <hr />
+          {subtitle}
+          <hr />
+          {description}
+        </Modal.Body>
+        <Modal.Footer className="custom-modal-footer">
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            <a href={visitUrl} target="_blank" rel="noreferrer">
+              Visit Site
+            </a>
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
