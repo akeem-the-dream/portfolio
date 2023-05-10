0b9f1205c74e56d79c2c9e591c9d6f773ceb5ff1
akeembrisco <briscoakeem@gmail.com>
2023-05-10 18:43:05 -0400

    created a edux store

--- src/store/Reducer.js
+++ src/store/Reducer.js
@@ -1,10 +1,30 @@
 const initialState = {
   Darkmode: true,
+  modalInfo: {
+    showModal: false,
+    description: "",
+    imgUrls: "",
+    subtitle: "",
+    title: "",
+    visitUrl: "",
+  },
 };
 export const storeReducer = (state = initialState, action) => {
   switch (action.type) {
     case "TOGGLE":
-      return { Darkmode: !state?.Darkmode };
+      return { ...state, Darkmode: !state?.Darkmode };
+    case "DISPLAY_MODAL":
+      return {
+        ...state,
+        modalInfo: {
+          showModal: action?.showModal,
+          description: action?.description,
+          imgUrls: action?.imgUrls,
+          title: action?.title,
+          subtitle: action?.subtitle,
+          visitUrl: action?.visitUrl,
+        },
+      };
     default:
       return state;
   }
--- src/store/Selector.js
+++ src/store/Selector.js
@@ -1,10 +1,29 @@
-
 export const mapDispatchToProps = (dispatch) => {
   return {
     toggle: () => dispatch({ type: "TOGGLE" }),
+    displayModal: (
+      showModal = false,
+      description = "",
+      imgUrls = "",
+      subtitle = "",
+      title = "",
+      visitUrl = ""
+    ) =>
+      dispatch({
+        type: "DISPLAY_MODAL",
+        showModal,
+        description,
+        imgUrls,
+        subtitle,
+        title,
+        visitUrl,
+      }),
   };
 };
 
 export const mapStateToProps = (state) => {
-  return { Darkmode: state?.Darkmode };
+  return {
+    Darkmode: state?.Darkmode,
+    modalInfo: state?.modalInfo,
+  };
 };
